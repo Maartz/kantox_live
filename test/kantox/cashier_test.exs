0ed0b92e59d9c40a9a413d50929a3f25e0300760
William Martin <maartz@icloud.com>
2024-08-20 17:05:46 +0200

    add tests data
    based on the provided pdf, this is the ExUnit implementation of them.

---
+++ test/kantox/cashier_test.exs
@@ -0,0 +1,26 @@
+defmodule SuperMarkex.CashierTest do
+  use ExUnit.Case
+  alias SuperMarkex.Cashier
+
+  describe "calculate_total/1" do
+    test "calculates total price for basket: GR1,SR1,GR1,GR1,CF1" do
+      basket = ["GR1", "SR1", "GR1", "GR1", "CF1"]
+      assert Cashier.calculate_total(basket) == Decimal.new("22.45")
+    end
+
+    test "calculates total price for basket: GR1,GR1" do
+      basket = ["GR1", "GR1"]
+      assert Cashier.calculate_total(basket) == Decimal.new("3.11")
+    end
+
+    test "calculates total price for basket: SR1,SR1,GR1,SR1" do
+      basket = ["SR1", "SR1", "GR1", "SR1"]
+      assert Cashier.calculate_total(basket) == Decimal.new("16.61")
+    end
+
+    test "calculates total price for basket: GR1,CF1,SR1,CF1,CF1" do
+      basket = ["GR1", "CF1", "SR1", "CF1", "CF1"]
+      assert Cashier.calculate_total(basket) == Decimal.new("30.57")
+    end
+  end
+end
