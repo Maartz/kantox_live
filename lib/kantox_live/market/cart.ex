03e26401c506a25a3b9b2f8e437f596f248dc309
William MARTIN <wmartin@dicksondata.fr>
2024-08-22 15:57:31 +0200

    add a cart behaviour
    As I'll use a GenServer for the Cart, I think it's a good idea to
    leverage a behaviour to have a clearer exposed API.

--- lib/kantox_live/market/cart.ex
+++ lib/kantox_live/market/cart.ex
@@ -1,2 +1,14 @@
 defmodule SuperMarkex.Market.Cart do
+  @type t :: %__MODULE__{
+          id: String.t(),
+          items: %{String.t() => non_neg_integer()}
+        }
+
+  @callback add_item(pid(), String.t(), non_neg_integer()) :: :ok | {:error, String.t()}
+  @callback remove_item(pid(), String.t(), non_neg_integer()) :: :ok | {:error, String.t()}
+  @callback get_items(pid()) :: %{String.t() => non_neg_integer()}
+  @callback clear(pid()) :: :ok
+
+  defstruct [:id, items: %{}]
 end
+
